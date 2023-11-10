6394b6cb6728e785472d1d3a80131254ced93541
Sun-ny11 <spetrow79@gmail.com>
2023-11-10 12:09:17 +0300

    lesson 1-10, is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { Tehnologies } from './components/Tehnologies';
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+      <Header/>
+      <Tehnologies/>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header =() => {
+   return (
+      <div>
+         <a href="">Home</a>
+         <a href="">Nwes Feed</a>
+         <a href="">Messages</a>
+      </div>
+   );
+};
